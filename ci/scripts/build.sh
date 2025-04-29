a816a7da73e0a561b1cd73ab1796d0cf266ef38b
Nick Lupton <19624419+mr-nick17@users.noreply.github.com>
2025-04-29 15:47:36 +0100

    update build script in CI to use git versioning in directory path

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,5 +1,16 @@
 #!/bin/bash -eux
 
+
 pushd dis-design-system-go
   make build
+  TAG=`git tag --points-at HEAD | grep ^v | head -n 1`
+  
+  if [ -z "${TAG}" ]; then
+    echo "No tag found, using short commit hash"
+    TAG=`git rev-parse --short HEAD`
+  fi
+
 popd
+
+mkdir build/$TAG
+cp -a dis-design-system-go/dist/assets/. build/$TAG
