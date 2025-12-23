4c5c45226fc8b27cec2d00404135ea5f68909c74
Nick Lupton <19624419+mr-nick17@users.noreply.github.com>
2025-12-23 11:28:46 +0000

    remove unreachable template and model props

--- model/page.go
+++ model/page.go
@@ -4,41 +4,40 @@ import (
 	"html/template"
 )
 
+// TODO: Refactor page struct into Config, Components, etc. structs
 // Page contains data re-used for each page type a Data struct for data specific to the page type
 type Page struct {
-	Count                            int              `json:"count"`
-	Type                             string           `json:"type"`
-	DatasetId                        string           `json:"dataset_id"`
-	DatasetTitle                     string           `json:"dataset_title"`
-	URI                              string           `json:"uri"`
-	Taxonomy                         []TaxonomyNode   `json:"taxonomy"`
-	Breadcrumb                       []TaxonomyNode   `json:"breadcrumb"`
-	IsInFilterBreadcrumb             bool             `json:"is_in_filter_breadcrumb"`
-	ServiceMessage                   string           `json:"service_message"`
-	Metadata                         Metadata         `json:"metadata"`
-	SearchDisabled                   bool             `json:"search_disabled"`
-	SiteDomain                       string           `json:"-"`
-	PatternLibraryAssetsPath         string           `json:"pattern_library_assets_path"`
-	Language                         string           `json:"language"`
-	IncludeAssetsIntegrityAttributes bool             `json:"-"`
-	ReleaseDate                      string           `json:"release_date"`
-	BetaBannerEnabled                bool             `json:"beta_banner_enabled"`
-	CookiesPreferencesSet            bool             `json:"cookies_preferences_set"`
-	CookiesPolicy                    CookiesPolicy    `json:"cookies_policy"`
-	HasJSONLD                        bool             `json:"has_jsonld"`
-	FeatureFlags                     FeatureFlags     `json:"feature_flags"`
-	Error                            Error            `json:"error"`
-	EmergencyBanner                  EmergencyBanner  `json:"emergency_banner"`
-	Collapsible                      Collapsible      `json:"collapsible"`
-	Pagination                       Pagination       `json:"pagination"`
-	TableOfContents                  TableOfContents  `json:"table_of_contents"`
-	BackTo                           BackTo           `json:"back_to"`
-	SearchNoIndexEnabled             bool             `json:"search_no_index_enabled"`
-	NavigationContent                []NavigationItem `json:"navigation_content"`
-	PreGTMJavaScript                 []template.JS    `json:"pre_gtm_javascript"`
-	RemoveGalleryBackground          bool             `json:"remove_gallery_background"`
-	Feedback                         Feedback         `json:"feedback"`
-	Enable500ErrorPageStyling        bool             `json:"enable_500_error_page_styling"` // flag for hiding standard page "furniture" (header, nav, etc.)
+	Count                     int              `json:"count"`
+	Type                      string           `json:"type"`
+	DatasetId                 string           `json:"dataset_id"`
+	DatasetTitle              string           `json:"dataset_title"`
+	URI                       string           `json:"uri"`
+	Taxonomy                  []TaxonomyNode   `json:"taxonomy"`
+	Breadcrumb                []TaxonomyNode   `json:"breadcrumb"`
+	ServiceMessage            string           `json:"service_message"`
+	Metadata                  Metadata         `json:"metadata"`
+	SearchDisabled            bool             `json:"search_disabled"`
+	SiteDomain                string           `json:"-"` // TODO: candidate for EnvironmentConfig struct
+	PatternLibraryAssetsPath  string           `json:"pattern_library_assets_path"`
+	Language                  string           `json:"language"`
+	ReleaseDate               string           `json:"release_date"`
+	BetaBannerEnabled         bool             `json:"beta_banner_enabled"`
+	CookiesPreferencesSet     bool             `json:"cookies_preferences_set"`
+	CookiesPolicy             CookiesPolicy    `json:"cookies_policy"`
+	HasJSONLD                 bool             `json:"has_jsonld"`
+	FeatureFlags              FeatureFlags     `json:"feature_flags"`
+	Error                     Error            `json:"error"`
+	EmergencyBanner           EmergencyBanner  `json:"emergency_banner"`
+	Collapsible               Collapsible      `json:"collapsible"`
+	Pagination                Pagination       `json:"pagination"`
+	TableOfContents           TableOfContents  `json:"table_of_contents"`
+	BackTo                    BackTo           `json:"back_to"`
+	SearchNoIndexEnabled      bool             `json:"search_no_index_enabled"`
+	NavigationContent         []NavigationItem `json:"navigation_content"`
+	PreGTMJavaScript          []template.JS    `json:"pre_gtm_javascript"`
+	RemoveGalleryBackground   bool             `json:"remove_gallery_background"`
+	Feedback                  Feedback         `json:"feedback"`
+	Enable500ErrorPageStyling bool             `json:"enable_500_error_page_styling"` // flag for hiding standard page "furniture" (header, nav, etc.)
 	ABTest
 }
 
@@ -56,12 +55,9 @@ type NavigationItem struct {
 
 // FeatureFlags contains toggles for certain features on the website
 type FeatureFlags struct {
-	EnableGetDataCard      bool   `json:"enable_get_data_card"`
-	HideCookieBanner       bool   `json:"hide_cookie_banner"`
-	ONSDesignSystemVersion string `json:"ons_design_system_version"`
-	EnableFeedbackAPI      bool   `json:"enable_feedback_api"` // Deprecated: EnableFeedbackAPI should not be used, it will be removed soon
-	FeedbackAPIURL         string `json:"feedback_api_url"`    // technically not a feature flag, but used exclusivly with one
-	IsPublishing           bool   `json:"is_publishing"`
+	HideCookieBanner bool   `json:"hide_cookie_banner"` // TODO: candidate for EnvironmentConfig struct
+	FeedbackAPIURL   string `json:"feedback_api_url"`   // TODO: candidate for EnvironmentConfig struct
+	IsPublishing     bool   `json:"is_publishing"`      // TODO: candidate for EnvironmentConfig struct
 }
 
 // NewPage instantiates the base Page type with configurable fields
